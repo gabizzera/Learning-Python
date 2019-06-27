5adb6b177096925e9cb2799274d89a36a49ac032
GABRIEL BENIGNO LUCHTENBERG MAFRA <52172302+gabizzera@users.noreply.github.com>
2019-06-26 22:28:58 -0300

    challenges

---
+++ desafio002.py
@@ -0,0 +1,6 @@
+nome = input("Qual seu nome?")
+dia = input("Qual dia voce nasceu:")
+mes = input("Que mes voce nasceu?")
+ano = input("Qual ano voce nasceu?")
+
+print("Bem vindo!",nome, "Voce nasceu dia", dia, "do mes", mes, "do ano de ",ano )
---
+++ desafio004.py
@@ -0,0 +1,7 @@
+Z = input('Digita algo ai:')
+
+print(type(Z))
+
+print('É numerico?',Z.isalnum())
+
+print('É um digito?',Z.isdigit())
