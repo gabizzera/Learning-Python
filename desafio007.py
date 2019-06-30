a868f09e100bfa1ed7718112e4ef4e024b9e44b3
GABRIEL BENIGNO LUCHTENBERG MAFRA <52172302+gabizzera@users.noreply.github.com>
2019-06-30 15:07:59 -0300

    Create desafio007.py

---
+++ desafio007.py
@@ -0,0 +1,9 @@
+nome = input('qual seu nome: ')
+
+n1 = float(input('Digite sua primeira nota: '))
+
+n2 = float(input('Digite sua primeira nota: '))
+
+m = (n1 + n2)/2 
+
+print('A media do aluno {} é {} '.format(nome,m))
