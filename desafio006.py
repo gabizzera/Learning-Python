c833f2719b38a9794eccf5b3fe3760be1d2a92da
GABRIEL BENIGNO LUCHTENBERG MAFRA <52172302+gabizzera@users.noreply.github.com>
2019-06-30 15:03:31 -0300

    Create desafio006.py

---
+++ desafio006.py
@@ -0,0 +1,7 @@
+n1 = float(input('Digite um numero: '))
+
+dobro = n1 * 2
+triplo = n1 * 3
+raiz = n1 ** (1/2)
+
+print('Seu numero é {} seu dobro é {} seu triplo {} e a raiz {}'.format(n1,dobro,triplo,raiz))
